732993548e544a727767ea9e03d58a12e4d5e14b
Doug K <doug.kerzner@gmail.com>
2021-01-15 17:07:10 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ require 'pry'
 
 def square_array(array)
   new_array = []
-  array.length.times { |square|
+ while array.length.times == square { |square|
   new_array.push (array[square] ** square)
   }
   new_array
